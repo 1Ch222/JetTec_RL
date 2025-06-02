e9ab763846dbcd185d95e15440deddbb5949637a
1Ch222 <137825924+1Ch222@users.noreply.github.com>
2025-06-02 12:20:44 -0600

    Create training_path.launch.py

---
+++ launch/training_path.launch.py
@@ -0,0 +1,25 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
+
+def generate_launch_description():
+    return LaunchDescription([
+        DeclareLaunchArgument('load', default_value='', description='Chemin vers un modèle à charger'),
+
+        Node(
+            package='jettec_robot',
+            executable='vision_path_node',
+            name='vision_path_node',
+            output='screen',
+        ),
+
+        Node(
+            package='jettec_robot',
+            executable='training_path_node',
+            name='training_path_node',
+            output='screen',
+            parameters=[{'use_sim_time': True}],
+            arguments=['--load', LaunchConfiguration('load')],
+        ),
+    ])
